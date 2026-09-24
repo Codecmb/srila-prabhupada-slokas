60be6aca866ee8f57dd719edad4626dc49ecbd75
Codecmb <92221983+Codecmb@users.noreply.github.com>
2026-09-24 17:31:12 -0400

    Update App 2 indigo gold design

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='prabhupada-slokas-complete-v1';
+const CACHE='prabhupada-slokas-indigo-v2';
 const CORE=['./','./index.html','./verses.json','./audio-map.json','./manifest.json','./icon-192.png','./icon-512.png','./srila-prabhupada.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
